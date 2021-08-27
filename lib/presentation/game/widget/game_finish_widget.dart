24a3d8e4e938b38333b38cb0cf1bada17f7214c2
oberdancunha <oberdancunha@gmail.com>
2021-08-27 16:48:27 -0300

    Sound of celebration when finishing the game.

--- lib/presentation/game/widget/game_finish_widget.dart
+++ lib/presentation/game/widget/game_finish_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
+import 'package:soundpool/soundpool.dart';
 
 class GameFinishWidget extends StatefulWidget {
   const GameFinishWidget({Key? key}) : super(key: key);
@@ -10,16 +12,23 @@ class GameFinishWidget extends StatefulWidget {
 
 class _GameFinishWidgetState extends State<GameFinishWidget> with TickerProviderStateMixin {
   late final AnimationController _controller;
+  late Soundpool _pool;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(vsync: this);
+    _pool = Soundpool.fromOptions(
+      options: const SoundpoolOptions(
+        streamType: StreamType.notification,
+      ),
+    );
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _pool.dispose();
     super.dispose();
   }
 
@@ -28,10 +37,22 @@ class _GameFinishWidgetState extends State<GameFinishWidget> with TickerProvider
         'assets/lotties/game_finish.json',
         fit: BoxFit.cover,
         controller: _controller,
-        onLoaded: (composition) {
+        onLoaded: (composition) async {
+          await _playGameFinishAudio(composition.duration);
           _controller
             ..duration = composition.duration
             ..forward();
         },
       );
+
+  Future<void> _playGameFinishAudio(Duration duration) async {
+    final soundId = await rootBundle.load('assets/sounds/game_finish.mp3').then(_pool.load);
+    final streamId = await _pool.play(soundId);
+    _pool.setVolume(
+      soundId: soundId,
+      streamId: streamId,
+      volume: 0.5,
+    );
+    Future.delayed(duration, () => _pool.stop(streamId));
+  }
 }
