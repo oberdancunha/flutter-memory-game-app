aca84307c42cb16e7ce5127fd6d500c8217c55bc
oberdancunha <oberdancunha@gmail.com>
2021-08-31 00:17:15 -0300

    Change background image on header widget.

--- lib/presentation/core/widgets/header_widget.dart
+++ lib/presentation/core/widgets/header_widget.dart
@@ -10,32 +10,7 @@ class HeaderWidget extends StatelessWidget {
         child: SizedBox(
           height: ScreenUtil().setHeight(120),
           width: double.infinity,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    colorFilter: ColorFilter.mode(
-                      Colors.transparent.withOpacity(0.3),
-                      BlendMode.dstIn,
-                    ),
-                    image: const AssetImage('assets/images/memory_game_background.png'),
-                  ),
-                ),
-              ),
-              Text(
-                'Jogo da Memória',
-                style: TextStyle(
-                  fontSize: ScreenUtil().setSp(50),
-                  fontFamily: 'K26ToyBlocks123',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.brown.shade700,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
+          child: Image.asset('assets/images/memory_game_background.png'),
         ),
       );
 }
